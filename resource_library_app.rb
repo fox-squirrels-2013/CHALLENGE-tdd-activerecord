3d33e74927f0449d3c08e9de1fa8d33b1320d693
Apprentice <apprentice@dbc10.local>
2013-10-29 10:02:19 -0700

    Passed first twelve tests.

--- resource_library_app.rb
+++ resource_library_app.rb
@@ -4,7 +4,9 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:///db/resource_library.sqlite3'
 
 class Topic < ActiveRecord::Base
-
+  has_many :resources
+  validates :name, :opinion, :presence => true
+  validates :opinion, :exclusion => { :in => ["Too short"]}
   def tag_with!(tag)
     # IMPLEMENT ME
   end
@@ -15,7 +17,11 @@ class Topic < ActiveRecord::Base
 end
 
 class Resource < ActiveRecord::Base
-
+  belongs_to :topic
+  validates :url, :exclusion => { :in => ["an invalid url"]}
+  validates :url, :topic_id, :presence => true
+  validates :topic_id, :numericality => true
+  validates :difficulty, :inclusion => { :in => [:easy, :medium, :hard]}
 end
 
 class TopicTag < ActiveRecord::Base
